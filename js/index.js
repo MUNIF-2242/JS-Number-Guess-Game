34b4a5c8854cb83af4bb23533cabde820f49d35c
MUNIF <munif.shellbeehaken@gmail.com>
2023-02-01 10:12:47 +0600

    local storage functionality done.

--- js/index.js
+++ js/index.js
@@ -5,11 +5,12 @@ function generateRandomNumbers(min, max) {
 }
 
 function startGame() {
-  let level = 1;
+  let level = getLevel();
+  //let level = 1;
   let count = 0;
   let score = 0;
   const levelTarget = generateRandomNumbers(45, 50);
-  let levelMaxAttempt = generateRandomNumbers(15, 20);
+  let levelMaxAttempt = generateRandomNumbers(30, 35);
   let levelText = document.getElementById('level');
   let attemptsRemainingText = document.getElementById('attempts-remaining');
   let bottomWarningText = document.getElementById('bottom-warning-text');
@@ -35,18 +36,18 @@ function startGame() {
   btn.onclick = function () {
     count = count + 1;
     let generatedNumber = generateRandomNumbers(0, 5);
-    console.log(`generated number ${generatedNumber}`);
+    //console.log(`generated number ${generatedNumber}`);
 
     if (score + generatedNumber <= levelTarget) {
       score = score + generatedNumber;
-      console.log(`score ${score}`);
+      //console.log(`score ${score}`);
       Validation();
     } else {
       score = score;
       Validation();
     }
 
-    console.log(`Score ${score}`);
+    //console.log(`Score ${score}`);
 
     attemptsRemainingText.innerHTML = `Attempts remaining:${
       levelMaxAttempt - count
@@ -56,13 +57,12 @@ function startGame() {
   };
 
   function Validation() {
-    if (count <= levelMaxAttempt) {
+    if (count < levelMaxAttempt) {
       if (score == levelTarget) {
         //showSuccessModal();
         setTimeout(showSuccessModal, 100);
       }
-    }
-    if (count == levelMaxAttempt) {
+    } else {
       //setTimeout(showFailureModal, 100);
       showFailureModal();
     }
@@ -80,18 +80,27 @@ function startGame() {
   };
 
   okBtn.onclick = function () {
-    // level++;
-    // localStorage.setItem('level', level);
+    level++;
+    localStorage.setItem('level', level);
     window.location.reload();
   };
 }
 function getLevel() {
   let x = localStorage.getItem('level');
-  document.getElementById('level').innerHTML = x;
-  console.log(`level ${x}`);
+  //document.getElementById('level').innerHTML = x;
+  if (x == null) {
+    return (x = 1);
+  }
+  //console.log(`level ${x}`);
+  else return x;
 }
 window.onload = function () {
   //localStorage.clear();
   startGame();
   //getLevel();
+  //console.log('on load');
+  //document.getItem('level');
 };
+// document.addEventListener('DOMContentLoaded', () => {
+//   console.log('Hello World!');
+// });
